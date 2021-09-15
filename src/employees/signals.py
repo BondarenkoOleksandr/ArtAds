3ca2c7a3a3843fbd9f552d29f3cd5db84115c93d
BondarenkoAlexander <sanyasashkabond32@gmail.com>
2021-09-15 12:42:19 +0300

    changed user roles

--- src/employees/signals.py
+++ src/employees/signals.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.models import User
+
 from django.db.models.signals import m2m_changed
 
 from core.utils import moderate_user_role
+from src.users.models import User
 
 
 def handle_m2m_changed(sender, instance, action, **kwargs):
