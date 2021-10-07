9b5af10a7b8fee734bcbaee87bedbe5e6c563c43
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-07 10:29:16 +0300

    google trans

--- src/articles/signals.py
+++ src/articles/signals.py
@@ -2,7 +2,7 @@ from autoslug.utils import slugify
 from django.core.exceptions import ValidationError
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from googletrans import Translator
+from google_trans_new import google_translator
 
 from articles.models import Article
 
@@ -10,7 +10,7 @@ from articles.models import Article
 @receiver(post_save, sender=Article)
 def save_lesson(sender, instance, created, **kwargs):
     if created:
-        translator = Translator()
-        value = translator.translate(instance.title)
+        translator = google_translator()
+        value = translator.translate(instance.title, lang_tgt='en')
         instance.slug = slugify(value)
         instance.save()
--- src/services/signals.py
+++ src/services/signals.py
@@ -1,7 +1,7 @@
 from autoslug.utils import slugify
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from googletrans import Translator
+from google_trans_new import google_translator
 
 from services.models import ServiceArticle
 
@@ -9,7 +9,7 @@ from services.models import ServiceArticle
 @receiver(post_save, sender=ServiceArticle)
 def save_lesson(sender, instance, created, **kwargs):
     if created:
-        translator = Translator()
-        value = translator.translate(instance.title)
+        translator = google_translator()
+        value = translator.translate(instance.title, lang_tgt='en')
         instance.slug = slugify(value)
         instance.save()
