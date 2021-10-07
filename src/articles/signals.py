34db07ecfbc4c131acfc01e0370e61e6f151fc27
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-07 10:08:08 +0300

    Translation model

--- src/articles/signals.py
+++ src/articles/signals.py
@@ -2,6 +2,7 @@ from autoslug.utils import slugify
 from django.core.exceptions import ValidationError
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from googletrans import Translator
 
 from articles.models import Article
 
@@ -9,6 +10,7 @@ from articles.models import Article
 @receiver(post_save, sender=Article)
 def save_lesson(sender, instance, created, **kwargs):
     if created:
-        value = instance.title
+        translator = Translator()
+        value = translator.translate(instance.title)
         instance.slug = slugify(value)
         instance.save()
--- src/services/signals.py
+++ src/services/signals.py
@@ -1,6 +1,7 @@
 from autoslug.utils import slugify
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from googletrans import Translator
 
 from services.models import ServiceArticle
 
@@ -8,6 +9,7 @@ from services.models import ServiceArticle
 @receiver(post_save, sender=ServiceArticle)
 def save_lesson(sender, instance, created, **kwargs):
     if created:
-        value = instance.title
+        translator = Translator()
+        value = translator.translate(instance.title)
         instance.slug = slugify(value)
         instance.save()
