40204005a9b6bf136e10e1d95a50ca373d92e4e6
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-11 12:37:31 +0300

    seo field for models

--- src/seo/models.py
+++ src/seo/models.py
@@ -6,7 +6,6 @@ from tinymce.models import HTMLField
 
 
 class SEO(models.Model):
-    content_object = GenericForeignKey()
     seo_title = models.CharField(max_length=500, null=True, blank=True)
     seo_description = HTMLField(max_length=500, null=True, blank=True)
     seo_canonical = models.URLField(null=True, blank=True)
