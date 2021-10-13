348cee5927161f726b3bd6466bc3e0642ddf165c
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-13 18:31:38 +0300

    Change admin panel

--- src/ArtAds/settings/base.py
+++ src/ArtAds/settings/base.py
@@ -230,13 +230,13 @@ AUTH_USER_MODEL = 'users.User'
 
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "Garage Doors MR",
+    "site_title": "Art Ads",
 
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "Garage Doors MR",
+    "site_header": "Art Ads",
 
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "Garage Doors MR",
+    "site_brand": "Art Ads",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
     "site_logo": "img/logo.png",
