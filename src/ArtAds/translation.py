82c414cc5fd04a62dd533dabfca1eb34e48c9e25
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-13 12:08:44 +0300

    Add translations field for services

--- src/ArtAds/translation.py
+++ src/ArtAds/translation.py
@@ -1,7 +1,7 @@
 from modeltranslation.translator import translator, TranslationOptions
 from articles.models import Article, ArticleCategory
 from employees.models import Employee, Skill
-from services.models import ServiceArticle, WorkPoint
+from services.models import ServiceArticle, WorkPoint, ServiceCategory
 
 
 class ArticleTranslationOptions(TranslationOptions):
@@ -10,6 +10,10 @@ class ArticleTranslationOptions(TranslationOptions):
 
 class ServiceArticleTranslationOptions(TranslationOptions):
     fields = ('title', 'text')
+
+
+class ServiceTranslationOptions(TranslationOptions):
+    fields = ('name', )
 
 
 class WorkPointTranslationOptions(TranslationOptions):
@@ -31,6 +35,7 @@ class ArticleCategoryTranslationOptions(TranslationOptions):
 translator.register(Article, ArticleTranslationOptions)
 translator.register(WorkPoint, WorkPointTranslationOptions)
 translator.register(ServiceArticle, ServiceArticleTranslationOptions)
+translator.register(ServiceCategory, ServiceTranslationOptions)
 translator.register(Employee, EmployeesTranslationOptions)
 translator.register(Skill, EmployeeSkillsTranslationOptions)
 translator.register(ArticleCategory, ArticleCategoryTranslationOptions)
