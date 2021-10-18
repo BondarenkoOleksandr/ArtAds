c807dd5546c23d05a4261bafeab1d34e3c70fe19
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-18 11:53:27 +0300

    created gallery

--- src/articles/utils.py
+++ src/articles/utils.py
@@ -3,5 +3,5 @@ from gallery.models import Photo
 
 def upload_photo_to_gallery(queryset):
     for model in queryset:
-        Photo.objects.get_or_create(image=model.image, title=model.title, alt=model.title)
-        Photo.objects.get_or_create(image=model.bg_image, title='BG -'+model.title, alt='BG -'+model.title)
+        Photo.objects.get_or_create(photo=model.image, title=model.title, alt=model.title)
+        Photo.objects.get_or_create(photo=model.bg_image, title='BG -'+model.title, alt='BG -'+model.title)
