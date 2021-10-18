6af4505d835d9fe176304931f95f63a1b86cd715
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-18 13:41:58 +0300

    Created function for cases review

--- src/cases/utils.py
+++ src/cases/utils.py
@@ -3,7 +3,8 @@ from gallery.models import Photo
 
 def upload_photo_to_gallery(queryset):
     for model in queryset:
-        Photo.objects.get_or_create(photo=model.mob_image, title=model.title, alt=model.title,
-                                    title_ru=model.title, alt_ru=model.title)
-        Photo.objects.get_or_create(photo=model.full_image, title=model.title, alt=model.title,
-                                    title_ru=model.title, alt_ru=model.title)
+        Photo.objects.get_or_create(photo=model.image,
+                                    title=model.name+'.',
+                                    alt=model.name+'.',
+                                    title_ru=model.name+'.',
+                                    alt_ru=model.name+'.')
