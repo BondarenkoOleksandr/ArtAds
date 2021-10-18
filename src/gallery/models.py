d2358ad13eed6628352b2d636ec66c5fde69fd0b
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-18 11:40:41 +0300

    created gallery

---
+++ src/gallery/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils.html import mark_safe
+# Create your models here.
+
+
+class Photo(models.Model):
+    photo = models.ImageField(upload_to='gallery')
+    alt = models.CharField(max_length=250)
+    title = models.CharField(max_length=250)
+
+    def image_tag(self):
+        return mark_safe('<img src="/media/%s" width="150" height="150" />' % self.photo)
+
+    image_tag.short_description = 'Preview'
+    image_tag.allow_tags = True
+
+    def __str__(self):
+        return self.title + ' - ' + str(self.id)
