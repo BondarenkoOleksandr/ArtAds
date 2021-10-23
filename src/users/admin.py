51977236eef335b78dace7cce8419e6f96734ef0
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-23 14:47:37 +0300

    admin changes

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
 from users.models import User
 
 
-admin.site.register(User)
+class UserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'image')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+admin.site.register(User, UserAdmin)
